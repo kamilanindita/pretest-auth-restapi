111edbc74ee8cf4fc76bf5237114670488e549ca
Kamil Anindita <55023703+kamilanindita@users.noreply.github.com>
2021-08-12 07:36:00 +0700

    deploy heroku config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //database connection
-mongoose.connect('mongodb://localhost:27017/securing-rest-apis-with-jwt', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://node:adminganteng@cluster0.wdyin.mongodb.net/securing-rest-apis-with-jwt?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err) { 
         console.log('MongoDB Connection Succeeded.') 
     }
